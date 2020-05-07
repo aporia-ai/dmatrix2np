0b42c950e7f063859e05f63e220ad91ca6d71d0e
Alon Gubkin <alon.gubkin@gmail.com>
2020-05-07 12:47:32 +0300

    lots of devops

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dmatrix2np
+
+[![Tests](https://github.com/aporia-ai/dmatrix2np/workflows/Test/badge.svg)](https://github.com/aporia-ai/dmatrix2np/actions?workflow=Test)
+
+[![PyPI](https://img.shields.io/pypi/v/dmatrix2np.svg)](https://pypi.org/project/dmatrix2np/)
--- dmatrix2np/__init__.py
+++ dmatrix2np/__init__.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+# Single source the package version
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
 
 def dmatrix_to_numpy(dmatrix):
     return np.zeros((3, 3))
